520fba7d146ce9aa7c437a1ab773dce0b00d3979
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-01-15 22:56:01 +0900

    Add Auth

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+import { AuthProvider } from '../contexts/Auth'
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+}
+
+export { App }
