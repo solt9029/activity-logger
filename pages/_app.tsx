25d41644a09a5026d9816e14c14ecf877d0e2d8c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-01-16 15:10:07 +0900

    Style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { AuthProvider } from '../contexts/Auth'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './_app.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
