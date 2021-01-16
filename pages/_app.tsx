3c502b2ef2dfbd77db12221d89de036119a43e43
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-01-16 14:40:28 +0900

    Mock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import { AppProps } from 'next/app'
 import { AuthProvider } from '../contexts/Auth'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
+
+const buttonStyle: CSSProperties = {
+  position: 'fixed',
+  width: '60px',
+  height: '60px',
+  bottom: '1rem',
+  right: '1rem',
+  backgroundColor: '#000',
+  color: '#fff',
+  borderRadius: '50px',
+  textAlign: 'center',
+  boxShadow: '2px 2px 3px #999',
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+      <div style={buttonStyle}>
+        <Link href="/new">
+          <FontAwesomeIcon icon={faPlus} style={{ marginTop: '18px', fontSize: '1.5rem' }} />
+        </Link>
+      </div>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { faPlay, faPlayCircle, faStopCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useContext, useEffect, useState } from 'react'
+import { Card, Col, Container, Navbar, NavbarBrand, Row } from 'react-bootstrap'
 import { AuthContext } from '../contexts/Auth'
 import { useDatabaseRef } from '../utils/hooks'
 
@@ -23,11 +26,62 @@ export default function IndexPage() {
   }
 
   return (
-    <div>
-      <button onClick={handleClick}>add</button>
-      <ul>
-        <div>{value.title}</div>
-      </ul>
-    </div>
+    <>
+      <Navbar variant="dark" bg="dark" className="mb-3">
+        <Container>
+          <NavbarBrand>Activity Logger</NavbarBrand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row className="mb-5">
+          <Col xs={12}>
+            <Card>
+              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
+                <FontAwesomeIcon icon={faStopCircle} style={{ marginRight: '1rem' }} />
+                14時28分に「仕事」を開始しました
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Col xs={12}>これから始める活動を選択してください</Col>
+        </Row>
+        <Row>
+          <Col xs={12} lg={6} style={{ marginBottom: '1rem' }}>
+            <Card>
+              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
+                <FontAwesomeIcon icon={faPlayCircle} style={{ marginRight: '1rem' }} />
+                仕事
+                <div style={{ float: 'right' }}>
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} lg={6} style={{ marginBottom: '1rem' }}>
+            <Card>
+              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
+                <FontAwesomeIcon icon={faPlayCircle} style={{ marginRight: '1rem' }} />
+                睡眠
+                <div className="float-right">
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} lg={6} style={{ marginBottom: '1rem' }}>
+            <Card>
+              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
+                <FontAwesomeIcon icon={faPlayCircle} style={{ marginRight: '1rem' }} />
+                読書
+                <div style={{ float: 'right' }}>
+                  <FontAwesomeIcon icon={faTrash} />
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
