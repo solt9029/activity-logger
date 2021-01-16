a086e7c5edeb0e7a713e5fb033e5f0cc5cc8254c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-01-16 14:55:35 +0900

    Move button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
-import { faPlay, faPlayCircle, faStopCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPlayCircle, faPlus, faStopCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useContext, useEffect, useState } from 'react'
+import Link from 'next/link'
+import { CSSProperties, useContext, useEffect, useState } from 'react'
 import { Card, Col, Container, Navbar, NavbarBrand, Row } from 'react-bootstrap'
 import { AuthContext } from '../contexts/Auth'
 import { useDatabaseRef } from '../utils/hooks'
+
+const buttonStyle: CSSProperties = {
+  position: 'fixed',
+  width: '60px',
+  height: '60px',
+  bottom: '1rem',
+  right: '1rem',
+  backgroundColor: '#000',
+  color: '#fff',
+  borderRadius: '50px',
+  textAlign: 'center',
+  boxShadow: '2px 2px 3px #999',
+}
 
 export default function IndexPage() {
   const { currentUser } = useContext(AuthContext)
@@ -36,9 +50,14 @@ export default function IndexPage() {
         <Row className="mb-5">
           <Col xs={12}>
             <Card>
-              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
-                <FontAwesomeIcon icon={faStopCircle} style={{ marginRight: '1rem' }} />
-                14時28分に「仕事」を開始しました
+              <Card.Body style={{ padding: '1rem' }}>
+                <div>
+                  <FontAwesomeIcon icon={faStopCircle} style={{ marginRight: '1rem' }} />
+                  14時28分に「仕事」を開始しました
+                </div>
+                <div className="text-secondary small" style={{ marginLeft: '2rem' }}>
+                  3時間24分30秒が経過しました
+                </div>
               </Card.Body>
             </Card>
           </Col>
@@ -49,7 +68,7 @@ export default function IndexPage() {
         <Row>
           <Col xs={12} lg={6} style={{ marginBottom: '1rem' }}>
             <Card>
-              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
+              <Card.Body style={{ padding: '1rem' }}>
                 <FontAwesomeIcon icon={faPlayCircle} style={{ marginRight: '1rem' }} />
                 仕事
                 <div style={{ float: 'right' }}>
@@ -60,7 +79,7 @@ export default function IndexPage() {
           </Col>
           <Col xs={12} lg={6} style={{ marginBottom: '1rem' }}>
             <Card>
-              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
+              <Card.Body style={{ padding: '1rem' }}>
                 <FontAwesomeIcon icon={faPlayCircle} style={{ marginRight: '1rem' }} />
                 睡眠
                 <div className="float-right">
@@ -71,7 +90,7 @@ export default function IndexPage() {
           </Col>
           <Col xs={12} lg={6} style={{ marginBottom: '1rem' }}>
             <Card>
-              <Card.Body style={{ padding: '1rem', fontSize: '1rem' }}>
+              <Card.Body style={{ padding: '1rem' }}>
                 <FontAwesomeIcon icon={faPlayCircle} style={{ marginRight: '1rem' }} />
                 読書
                 <div style={{ float: 'right' }}>
@@ -81,6 +100,11 @@ export default function IndexPage() {
             </Card>
           </Col>
         </Row>
+        <div style={buttonStyle}>
+          <Link href="/new">
+            <FontAwesomeIcon icon={faPlus} style={{ marginTop: '18px', fontSize: '1.5rem' }} />
+          </Link>
+        </div>
       </Container>
     </>
   )
