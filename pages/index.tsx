6c814d889ee1d059c0e1eea504b6b1071585aea3
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-01-16 09:39:13 +0900

    Add database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
+import { useContext } from 'react'
+import { AuthContext } from '../contexts/Auth'
+import { firebase } from '../utils/firebase'
+
 export default function IndexPage(): JSX.Element {
-  return <div>index</div>
+  const { currentUser } = useContext(AuthContext)
+  console.log(currentUser?.uid)
+  return (
+    <div>
+      <button
+        onClick={() => {
+          firebase.database().ref(`current-activity/${currentUser?.uid}`).set({
+            title: 'work',
+            startedAt: '2020-10-12',
+            finishedAt: null,
+          })
+        }}
+      >
+        add
+      </button>
+      <ul></ul>
+    </div>
+  )
 }
